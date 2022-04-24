63287c3027009f315a7e99b42c167114a95218b4
FathurAnshari <anshariF17@gmail.com>
2022-04-24 08:42:34 +0700

    almost done

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage !== null) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
   renderLeads();
+  console.log(localStorage.getItem("myLeads"));
 });
 
 function renderLeads() {
@@ -18,7 +34,7 @@ function renderLeads() {
             ${myLeads[i]}
         </a>
     </li>`;
-    console.log(listItem);
+    // console.log(listItem);
   }
   ulEl.innerHTML = listItem;
 }
